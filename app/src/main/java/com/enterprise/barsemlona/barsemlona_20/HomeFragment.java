0296e78752658d6b2dc72ce6c4132189eaeff0b1
Bar-semLona <barsemlonapi@gmail.com>
2017-05-24 21:53:16 -0300

    Enviar tudo

--- app/src/main/java/com/enterprise/barsemlona/barsemlona_20/HomeFragment.java
+++ app/src/main/java/com/enterprise/barsemlona/barsemlona_20/HomeFragment.java
@@ -1,29 +1,74 @@
 package com.enterprise.barsemlona.barsemlona_20;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.ArrayList;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class HomeFragment extends Fragment {
-
+    private ViewGroup mensagens;
+    String msg,prc,desc,cat,desconto,id;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+        View homeFragment = (View) inflater.inflate(R.layout.fragment_home, container, false);
+        mensagens = (ViewGroup) homeFragment.findViewById(R.id.container);
 
+        Bundle bundle = this.getArguments();
+        ArrayList<String> idProd = bundle.getStringArrayList("idProduto");
+        ArrayList<String> nome = bundle.getStringArrayList("nomeProduto");
+        ArrayList<String> preco = bundle.getStringArrayList("precoProduto");
+        ArrayList<String> desciption = bundle.getStringArrayList("descProduto");
+        ArrayList<String> categoria = bundle.getStringArrayList("categoriaProduto");
+        ArrayList<String> descontoP = bundle.getStringArrayList("descontoProduto");
+        for (int i = 0; i < preco.size(); i++) {
+            msg = nome.get(i);
+            prc = preco.get(i);
+            desc = desciption.get(i);
+            cat = categoria.get(i);
+            desconto = descontoP.get(i);
+            id = idProd.get(i);
+            addItem(msg, desc,prc,cat,desconto,id);
+        }
+        return  homeFragment;
+    }
+    private void addItem(final String nomeProduto,final String desc ,final String precoProduto,final String categoria,final String desconto,final String id) {
+        CardView cardView = (CardView) LayoutInflater.from(getContext()).inflate(R.layout.card_file_product, mensagens, false);
+        TextView titulo = (TextView) cardView.findViewById(R.id.titulo);
+        TextView mensagem = (TextView) cardView.findViewById(R.id.mensagem);
+        titulo.setText(nomeProduto);
+        mensagem.setText(precoProduto);
+        mensagens.addView(cardView);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragmentDetalhe = new DetalheFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame, fragmentDetalhe);
+                Bundle bundle = new Bundle();
+                bundle.putString("idProduto", id);
+                bundle.putString("nomeProduto", nomeProduto);
+                bundle.putString("precoProduto",precoProduto );
+                bundle.putString("descProduto", desc);
+                bundle.putString("categoriaProduto", categoria);
+                bundle.putString("descontoProduto",desconto);
+                fragmentDetalhe.setArguments(bundle);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        }) ;
+    }
 }
--- app/src/main/java/com/enterprise/barsemlona/barsemlona_20/LoginActivity.java
+++ app/src/main/java/com/enterprise/barsemlona/barsemlona_20/LoginActivity.java
@@ -1,13 +1,28 @@
 package com.enterprise.barsemlona.barsemlona_20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnContinuar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnContinuar = (Button) findViewById(R.id.btn_slogin);
+
+        btnContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,MenuActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
